c1fa323050b8f86f2e6cbce5d3a36fd898568a02
kimuyu <victorkimuyu@gmail.com>
2022-06-22 23:38:29 +0300

    Added Custom Font
    Added Custom Metropolic @font-face style sheet

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Metropolis', 'system-ui']
+      }
+    },
   },
   plugins: [],
 }
