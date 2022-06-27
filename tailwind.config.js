e83c276bda08f2bfcd0a488b4fc0ebf544ef35d1
kimuyu <victorkimuyu@gmail.com>
2022-06-27 18:05:03 +0300

    Established base style for table

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['Metropolis', 'system-ui']
+        'sans': ['Roboto', 'system-ui']
+      },
+      'colors': {
+        'bbg-blue': '#0b1d32',
+        'bbg-pink-400': '#ea1e4f',
+        'bbg-pink-600': '#c2185b',
+        'bbg-slate': '#292a30',
+        'bbg-blue-100': '#ebecf4',
+        'bbg-gray-100': '#f3f3f3',
+        'bbg-gray-400': '#696969',
+        'bbg-gray-800': '#262626',
+        'bbg-black': '#111111'
       }
     },
   },
